8fa90f184d1620fcec12173ab0f555a0813d7d62
mariusbike1983 <pepumariusdoru@gmail.com>
2023-07-09 11:40:50 +0300

    initial commit

---
+++ src/components/Item.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+    text: ""
+})
+</script>
+
+<template>
+    <div class="item">{{ props.text }}</div>
+</template>
+
+<style scoped>
+    .item {
+        background-color: lightskyblue;
+        color: black;
+    }
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Item from '../components/Item.vue';
+
+</script>
+
+<template>
+  <div class="about">
+    <Item :text="'This is the About page'"/>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
