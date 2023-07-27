e358af75809a10e33689906c3163e8593faca31d
MihaiV0 <131472508+MihaiV0@users.noreply.github.com>
2023-07-27 13:16:31 +0300

    added redirect to login in page not found

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,12 +1,21 @@
 <script setup>
+import router from '../router';
 
+function redirectToLogin() {
+    router.push('/login');
+}
 
 </script>
 
 <template>
-<div class="notFound">
-    <img src="../assets/img/404_Error_Page_not_Found.svg"/>
-</div>
+    <img 
+        id="aims"
+        src="../assets/img/AIMS.svg"
+        @click="redirectToLogin"
+    >
+    <div class="notFound">
+        <img src="../assets/img/404_Error_Page_not_Found.svg"/>
+    </div>
 
 </template>
 
@@ -15,5 +24,15 @@
         width: 50%;
         height: 50%;
         margin-left: 40vh;
+    }
+
+    #aims:hover {
+        cursor: pointer;
+    }
+
+    #aims {
+        position: absolute;
+        top: 5vh;
+        left: 5vw;
     }
 </style>
