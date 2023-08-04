8737ad1da1e1ead18358cc75573c24bc6fe888ee
MihaiV0 <131472508+MihaiV0@users.noreply.github.com>
2023-08-04 11:12:55 +0300

    fixed main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import mitt from 'mitt';
-const emitter = mitt();
+
 const app = createApp(App);
-app.config.globalProperties.emitter = emitter;
+
 app.use(router)
 
 app.mount('#app')
