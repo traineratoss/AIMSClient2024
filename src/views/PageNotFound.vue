cb39d456c5b9f0e55aea3b69420b0d960fc54e74
wolflorena <wolflorenaani@gmail.com>
2023-08-04 16:55:20 +0300

    Centered not found image

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,38 +1,37 @@
 <script setup>
-import router from '../router';
+import router from "../router";
 
 function redirectToLogin() {
-    router.push('/login');
+  router.push("/login");
 }
-
 </script>
 
 <template>
-    <img 
-        id="aims"
-        src="../assets/img/AIMS.svg"
-        @click="redirectToLogin"
-    >
-    <div class="notFound">
-        <img src="../assets/img/404_Error_Page_not_Found.svg"/>
-    </div>
-
+  <img id="aims" src="../assets/img/AIMS.svg" @click="redirectToLogin" />
+  <div class="notFound">
+    <img src="../assets/img/404_Error_Page_not_Found.svg" id="not-found" />
+  </div>
 </template>
 
 <style scoped>
-    .notFound{
-        width: 50%;
-        height: 50%;
-        margin-left: 40vh;
-    }
+.notFound {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-    #aims:hover {
-        cursor: pointer;
-    }
+#not-found {
+  height: 90vh;
+}
 
-    #aims {
-        position: absolute;
-        top: 5vh;
-        left: 5vw;
-    }
+#aims:hover {
+  cursor: pointer;
+}
+
+#aims {
+  position: absolute;
+  top: 5vh;
+  left: 5vw;
+}
 </style>
