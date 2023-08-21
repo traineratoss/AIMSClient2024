cd2e7cc753e69f2b343d8690766f5bd1fa5d2df6
rarestrifu <rarestrifu2001@yahoo.com>
2023-08-21 12:05:23 +0300

    all ideas view date modified

--- src/views/AllIdeasView.vue
+++ src/views/AllIdeasView.vue
@@ -689,9 +689,9 @@ async function showStatistics() {
               : { 'text-align': 'right' }
             ">
             <label for="sortOrder">Sort by: </label>
-            <select id="sortOrder" v-model="sortOrder" @change="updateSortOrder">
-              <option :value="0">Date ascending</option>
-              <option :value="1">Date descending</option>
+            <select id="sortOrder" v-model="sortOrder" @change="updateSortOrder" style="width: 3.8vw;">
+              <option :value="0"> Newest  </option>
+              <option :value="1"> Oldest</option>
             </select>
             <div class="pageSize">
               <PageSizeSelect id="pageSizeSelect" label="Ideas:" @change-display="changeDisplay" />
