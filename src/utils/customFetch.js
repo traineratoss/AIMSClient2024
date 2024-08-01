70ed496732ab908a374c063e4e26857dfe192d84
MurariuMarius <mam.murariu@gmail.com>
2024-08-01 11:05:49 +0300

    Refresh tokens when changing username cr: Bogdan

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -4,30 +4,45 @@ import { logout } from "../services/user_service"
 
 export const customFetch = async (url, options = {}) => {
 
+    options = {
+        ...options,
+        mode: "cors",
+        credentials: "include"
+    }
+
     if (isRefreshTokenExipred()) {
         await logout();
     }
 
     if (isAccessTokenExpired()) {
-        try {
-            const response = await nativeFetch('http://localhost:8080/api/v1/auth/refresh-token', {
-                method: "POST",
-                mode: "cors",
-                credentials: "include"
-            })
-            
-            const json = await response.json();
-
-            setTokenExpiry(json.accessTokenExpiryDate, json.refreshTokenExpiryDate);
-
-            console.log("Refreshed tokens")
-        } catch (error) {
-            console.log(error.message);
-            await logout();
-        }
+        await refreshTokens();
     }
 
     const response = await nativeFetch(url, { ...options });
 
+    if (url.includes("http://localhost:8080/users/update-profile")) {
+        await refreshTokens();
+    }
+
     return response;
+}
+
+
+const refreshTokens = async () => {
+    try {
+        const response = await nativeFetch('http://localhost:8080/api/v1/auth/refresh-token', {
+            method: "POST",
+            mode: "cors",
+            credentials: "include"
+        })
+        
+        const json = await response.json();
+
+        setTokenExpiry(json.accessTokenExpiryDate, json.refreshTokenExpiryDate);
+
+        console.log("Refreshed tokens")
+    } catch (error) {
+        console.log(error.message);
+        await logout();
+    }
 }
